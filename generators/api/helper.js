28344f4dbcac2a53acc6d17ceb0c9bc7cd575d75
David Skalka <david.skalka@gmail.com>
2024-05-07 18:10:22 +0200

    chore: Add support for Decimal and DateTime types in API generator and faker object initializer

--- generators/api/helper.js
+++ generators/api/helper.js
@@ -1,6 +1,9 @@
 const primitivesTypes= new Map();
 primitivesTypes.set('Integer', 'int');
 primitivesTypes.set('Text', 'string');
+primitivesTypes.set('Decimal', 'decimal');
+primitivesTypes.set('DateTime', 'DateTime');
+primitivesTypes.set('Bool', 'bool');
 
 const defaultValues= new Map().set("Text", `String.Empty`);
 
